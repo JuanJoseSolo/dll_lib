95d285d61b00f5743104ab3550ec0804e4300a88
JuanJoseSolo <juanjose.solorzano.c@gmail.com>
2024-06-10 04:04:07 +0200

    Added: <COMobj> - .dll has been created v2.

--- obj/Debug/net8.0/win-x86/dll_lib.AssemblyInfo.cs
+++ obj/Debug/net8.0/win-x86/dll_lib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dll_lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ba4a3ae2d636ca04428ee2092927355a956de5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b7714459afd7edd1c6d0b84921f264e648f05d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dll_lib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dll_lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
