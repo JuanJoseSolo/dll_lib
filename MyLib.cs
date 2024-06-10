57d42a1a24d7a4279a4959f04d16475d6d2f37af
JuanJoseSolo <juanjose.solorzano.c@gmail.com>
2024-06-10 06:43:43 +0200

    Updated: <Interface> - interface strategy added.

--- MyLib.cs
+++ MyLib.cs
@@ -3,13 +3,16 @@ using System.Runtime.InteropServices;
 namespace libApp{
     [Guid("ace9381f-cc28-4860-84ba-34331b1b498e")]
     [ClassInterface(ClassInterfaceType.None)]
-    public class MyComJLib : ITestApp{
+    public class MainJClass : ITestApp{
       public int TheValue;
       public void SetValue(int theValue){
-        TheValue = theValue + 10;
+        TheValue = theValue;
       }
       public int GetValue(){
         return TheValue;
+      }
+      public float Multiply(float _value){
+        return _value*TheValue;
       }
     }
 }
