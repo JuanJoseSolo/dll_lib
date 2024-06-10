85ba4a3ae2d636ca04428ee2092927355a956de5
JuanJoseSolo <juanjose.solorzano.c@gmail.com>
2024-06-10 02:05:21 +0200

    Initialized: <repo> - csharp library added.

---
+++ myDllLib.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+// A namespace is using to import libraries into another .cs file.
+namespace lib.app{
+  // Public class TestApp 
+  [ComVisible(true)]
+  public class TestApp{
+    public int TheValue;   
+    // public method.
+    public void SetValue(int theValue){
+      System.Console.WriteLine("Using 'SetValue' method ...");
+      TheValue = theValue;
+    }
+    public int GetValue(){
+      System.Console.WriteLine("Using 'GetValue' method ...");
+      return TheValue;
+    }
+  }
+}
